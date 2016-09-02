7f51fca933b9d2f06c7390bfd195e0f8997bbc9a
Adam Kelsey <adam.kelsey113@gmail.com>
2016-09-02 17:42:55 -0400

    additional routes set up to get all speakers, and individual speakers

--- app/app.js
+++ app/app.js
@@ -4,7 +4,18 @@ var dataFile = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000 );
 
+
+
 app.get('/', function(req, res) {
+  res.send(`
+      <h1>Welcome</h1>
+      <p>Roux Academy Meetups put together artists from all walks of life</p>
+  `);
+});
+
+
+
+app.get('/speakers', function(req, res) {
   var info = '';
   dataFile.speakers.forEach(function(item) {
     info += `
@@ -20,6 +31,29 @@ app.get('/', function(req, res) {
   `);
 });
 
+
+
+app.get('/speakers/:speakerid', function(req, res) {
+  
+  var speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+    <h1>${speaker.title}</h1>
+    <h2>with ${speaker.name}</h2>
+    <p>${speaker.summary}</p>
+  `);
+});
+
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
 });
+
+
+
+
+
+
+
+
+
+
+
