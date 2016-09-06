57331b5b57e49a92f155cce731d95e483171fc13
Adam Kelsey <adam.kelsey113@gmail.com>
2016-09-06 16:53:02 -0400

    routes modularized, used module exports and express router method

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -2,40 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/speakers', function(req, res) {
-  var data = req.app.get('appData');
-  var pagePhotos = [];
-  var pageSpeakers = data.speakers;
-
-  data.speakers.forEach(function(item) {
-    pagePhotos = pagePhotos.concat(item.artwork);
+  var info = '';
+  var dataFile = req.app.get('appData');
+  dataFile.speakers.forEach(function(item) {
+    info += `
+    <li>
+      <h2>${item.name}</h2>
+      <p>${item.summary}</p>
+    </li>
+    `;
   });
-
-  res.render('speakers', {
-    pageTitle: 'Speakers',
-    artwork: pagePhotos,
-    speakers: pageSpeakers,
-    pageID: 'speakerList'
-  });
+  res.send(`
+    <h1>Roux Academy Meetups</h1>
+    ${info}
+  `);
 });
+
 
 router.get('/speakers/:speakerid', function(req, res) {
-  var data = req.app.get('appData');
-  var pagePhotos = [];
-  var pageSpeakers = [];
-
-  data.speakers.forEach(function(item) {
-    if (item.shortname == req.params.speakerid) {
-      pageSpeakers.push(item);
-      pagePhotos = pagePhotos.concat(item.artwork);
-    }
-  });
-
-  res.render('speakers', {
-    pageTitle: 'Speaker Info',
-    artwork: pagePhotos,
-    speakers: pageSpeakers,
-    pageID: 'speakerDetail'
-  });
+  var dataFile = req.app.get('appData');
+  var speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+    <h1>${speaker.title}</h1>
+    <h2>with ${speaker.name}</h2>
+    <p>${speaker.summary}</p>
+  `);
 });
+
 
 module.exports = router;
