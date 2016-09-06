9f0b54f01a4dab7a2353cc776d8496dba2cae0a9
Adam Kelsey <adam.kelsey113@gmail.com>
2016-09-06 17:33:18 -0400

    used static method to use public folder to add images and styles across the project

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.send(`
       <h1>Welcome</h1>
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
       <p>Roux Academy Meetups put together artists from all walks of life</p>
   `);
 });
