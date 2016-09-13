578a53c623bf3360a263cebcfbcb3fc756568774
Adam Kelsey <adam.kelsey113@gmail.com>
2016-09-13 14:28:39 -0400

    added EJS template engine, changed homepage route to use index view

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send(`
-      <h1>Welcome</h1>
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-      <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
-      <p>Roux Academy Meetups put together artists from all walks of life</p>
-  `);
+
+  res.render('index');
+  
 });
 
 module.exports = router;
